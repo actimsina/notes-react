1217c993abfe9122835246f834208534aa5f7dbb
Achyut <actimsina@gmail.com>
2023-07-04 09:51:03 +0545

    login test with internal axios imports

--- src/components/Login.test.js
+++ src/components/Login.test.js
@@ -1,9 +1,10 @@
 import { logRoles, render, screen } from "@testing-library/react"
 import Login from "./Login"
+import { BrowserRouter } from "react-router-dom"
 
 describe('Login', () => {
     test('should render correctly', () => {
-        const view = render(<Login />)
+        const view = render(<BrowserRouter><Login /></BrowserRouter>)
         logRoles(view.container)
         const usernameInput = screen.getByPlaceholderText('Username')
         const passwordInput = screen.getByPlaceholderText('Password')
